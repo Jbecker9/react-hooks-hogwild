5113160e1b2f55b38493a6f2790deb49b6a49a74
Jared <jbeckersalax9@aol.com>
2022-02-12 12:54:03 -0500

    Done.

--- src/components/HogList.js
+++ src/components/HogList.js
@@ -2,18 +2,28 @@ import React from "react";
 import Hogs from "./Hogs";
 
 function HogList({ pigs }){
+    
+    function handleClick(data){
+        const selected = data.isSelected
+        const clickedHog = {...data, selected: !selected}
+        console.log(clickedHog)
+    }
     return (
+
 
         <div className="ui link cards">
             {pigs.map((hog)=>
-                <Hogs key={hog.name}
+                <Hogs 
+                handleClick={handleClick}
+                key={hog.name}
                 hog={hog}
                 hogName={hog.name} 
                 hogImage={hog.image} 
                 specialty={hog.specialty}
                 isSelected={hog.isSelected}
                 weight={hog.weight}
-                greased={hog.greased}/>)}
+                greased={hog.greased}
+                />)}
         </div>
     )
 }
--- src/components/Hogs.js
+++ src/components/Hogs.js
@@ -1,20 +1,19 @@
 import React from "react";
 import HogClickDisplay from "./HogClickDisplay";
 
-function Hogs({ hogName, hogImage, specialty, isSelected, weight, greased, hog }){
+function Hogs({ hogName, hogImage, specialty, isSelected, weight, greased, hog, handleClick }){
 
+    function processClick(){
+        handleClick(hog)
+    } 
 
-    function handleClick(){
-        const clickedHog = {...hog, isSelected: !isSelected}
-        console.log(clickedHog)
-    }
     
     return (
     <div key={hogName} id={hogName}className="card">
         <div key={hogName + "image"}className="image">
             <img src={hogImage} alt={hogName + "image"}/>
         </div>
-        <div onClick={handleClick} key={hogName + "content"}className="content">
+        <div onClick={processClick} key={hogName + "content"}className="content">
             <div key={hogName + "name"}className="header">{hogName}</div>
             {isSelected ? <HogClickDisplay specialty={specialty} weight={weight} greased={greased}/> : null}
         </div>
